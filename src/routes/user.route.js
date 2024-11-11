807498edef74cb4713d3fac092a0f25353f957e5
codify01 <amoleuthman@gmail.com>
2024-11-11 23:03:03 +0100

    vvv

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,20 +3,16 @@ const { registerUser, loginUser, loginWithFace } = require('../controllers/user.
 const router = express.Router()
 const multer = require('multer')
 
-// Configure multer for image uploads
 const upload = multer({ dest: 'uploads/' });
 
-// Route for user registration (image is optional for face recognition)
 router.post('/register', upload.single('image'), (req, res, next) => {
-    console.log("Image upload:", req.file); // Log the uploaded image details
-    next(); // Proceed to controller
+    console.log("Image upload:", req.file)
+    next()
 }, registerUser);
 
 
-// Route for login with email and password
 router.post('/login', loginUser);
 
-// Route for login with face recognition
 router.post('/login-with-face', upload.single('image'), loginWithFace);
 
 module.exports = router;
